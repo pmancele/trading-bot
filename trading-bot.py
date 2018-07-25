f131b6367dfe3d5df925e14d00619f77466bc495
Pierre-Marie Ancele <pm.ancele@catie.fr>
2018-07-25 17:58:33 +0200

    fix bug

--- trading-bot.py
+++ trading-bot.py
@@ -14,7 +14,7 @@ if not creds or creds.invalid:
 service = build('gmail', 'v1', http=creds.authorize(Http()))
 
 public_client = bitfinex.client.Public()
-
+trading_client = bitfinex.client.Trading(key='xxx', secret='xxx')
 
 def CreateMsgLabels():
   """Create object to update labels.
@@ -29,15 +29,22 @@ def executeOrder(messageSnippet):
         words = messageSnippet.split(" ")
         print("BUYING {}".format(words[6]))
         symbol = words[6] + 'USD'
-        print("Current price is {}".format(public_client.ticker(symbol)['last_price']))
+        ticker = public_client.ticker(symbol)
+        print("Current price is {}. Bid is {}".format(ticker['last_price'], ticker['bid']))
+        buy_price = float(ticker['bid']) + 0.001*float(ticker['bid'])
+        print("Putting BUY order of 50 USD on {} at price {}".format(symbol, buy_price))
         print()
+        return 0
 
-    elif 'SELL' in messageSnippet:
+    if 'SELL' in messageSnippet:
         words = messageSnippet.split(" ")
         print("SELLING {}".format(words[6]))
         symbol = words[6] + 'USD'
         print("Current price is {}".format(public_client.ticker(symbol)['last_price']))
         print()
+        return 0
+    else:
+        return -1
 
 if __name__ == '__main__':
     # Get last messages (1 day)
@@ -46,11 +53,13 @@ if __name__ == '__main__':
     # process mails if they are new and not already processed
     messages = query.get('messages')
     for message in messages:
-        message = service.users().messages().get(userId='me', id=message['id']).execute()
-        if 'INBOX' in message['labelIds']:
-            print("Alert found: {}".format(message['snippet']))
+        content = service.users().messages().get(userId='me', id=message['id']).execute()
+        if 'INBOX' in content['labelIds']:
+            print("Alert found: {}".format(content['snippet']))
             print("Placing order ...")
-            executeOrder(message['snippet'])
+            if (executeOrder(content['snippet']) == 0):
+                service.users().messages().modify(userId='me', id=content['id'], body=CreateMsgLabels()).execute()
+                print("Order executed")
 
-        if 'Label_3666501887904461799' in message['labelIds']:
-            print("Found alert already processed: {}".format(message['snippet']))
+        if 'Label_3666501887904461799' in content['labelIds']:
+            print("Found alert already processed: {}".format(content['snippet']))
